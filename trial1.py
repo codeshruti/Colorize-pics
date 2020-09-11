1295d5308a2200ef12a2747408b03233962f6721
S Shruti <54448597+codeshruti@users.noreply.github.com>
2020-09-11 22:49:00 +0530

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -1,6 +1,6 @@
 import Algorithmia
 input = {
-"image": "data:Desktop/holi.jpg " # Location of image
+"image": "data:Desktop/holi.jpeg " # Location of image
 }
 client = Algorithmia.client("#######") #insert your own API key
 algo = client.algo('deeplearning/ColorfulImageColorization/1.0.1')
