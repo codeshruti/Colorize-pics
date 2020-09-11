bfc53686836e343e20e2f2cab5b557f962432dbb
S Shruti <54448597+codeshruti@users.noreply.github.com>
2020-09-11 22:39:53 +0530

    Colorize code

---
+++ trial1.py
@@ -0,0 +1,7 @@
+import Algorithmia
+input = {
+"image": "data:Desktop/holi.jpg " # Location of image
+}
+client = Algorithmia.client("#######") #insert your own API key
+algo = client.algo('deeplearning/ColorfulImageColorization/1.0.1')
+print(algo.pipe(input))
